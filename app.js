848b70b8f19b1a1e283af1f3391ea2130bd197d2
Team8692 <rhsrobotics@outlook.com>
2017-06-04 14:08:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ var apibase = "https://rawgit.com/Team8692/team8692.github.io/master/";
 
 $(function() {
     var team = new Gh3.User("team8692"),
-        teamBlog = new Gh3.Repository("team8692.github.io", team);
+        teamBlog = new Gh3.Repository("team8692.github.io", team),
+        markdown = new showdown.Converter();
     teamBlog.fetch(function(err, res) {
         teamBlog.fetchBranches(function(err, res) {
             var master = teamBlog.getBranchByName("master");
             master.fetchContents(function(err, res) {
                 master.eachContent(function(content) {
-                    if(content.name.charAt(0) == '!') {
+                    if(content.name.charAt(0) == '#') {
                       $("<iframe />", {
+                        id: content.name,
                         src: apibase + content.name,
                         style: "display: none;"
-                      }).appendTo("body");  
+                      }).appendTo("body");
+                        
+                      $("body").append(markdown.makeHtml($(content.name)[0].getElementsByTag("pre")[0].innerHTML));
                     }
                 });
             });
