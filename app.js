f62f57fb4c17ceb363a60b6fbf9e0fe78255e970
Team8692 <rhsrobotics@outlook.com>
2017-06-04 13:23:46 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var apibase = "https://rawgit.com/Team8692/team8692.github.io/master/";
+var apibase = "https://cdn.rawgit.com/Team8692/team8692.github.io/53e581a9/";
 
 $(function(){
-	var team = new Gh3.User("team8692"), teamBlog = new Gh3.Repository("team8692.github.io", team);
-	teamBlog.fetch(function(err, res) {
-		teamBlog.fetchBranches(function(err, res) {
-			var master = teamBlog.getBranchByName("master");
-			master.fetchContents(function(err, res) {
-				master.eachContent(function(content) {
-					if(content.name.charAt(0) == '!') {
-						$("<iframe />", {
-							src: apibase+content.name,
-							style: "display: none;"
-						}).appendTo("body");
-					}
-				});
-			});
+  var team = new Gh3.User("team8692"), teamBlog = new Gh3.Repository("team8692.github.io", team);
+  teamBlog.fetch(function(err, res) {
+	  teamBlog.fetchBranches(function(err, res) {
+		  var master = teamBlog.getBranchByName("master");
+		  master.fetchContents(function(err, res) {
+		  master.eachContent(function(content) {
+			if(content.name.charAt(0) == '!') {
+			  $("<iframe />", {
+			  src: apibase+content.name,
+			  style: "display: none;"
+			}).appendTo("body");
+		  }
 		});
+	  });
 	});
+  });
 });
